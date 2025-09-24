f6a4590281a97627ffb81184b04e23176fe6d64b
aureliopagan <aurepgst@gmail.com>
2025-09-24 16:05:12 -0400

    feat: initialize TypeScript project with Webpack configuration
    - Added package.json with development dependencies for TypeScript, Webpack, and Jest.
    - Created tsconfig.json for TypeScript compiler options.
    - Set up webpack.config.js for module bundling, including TypeScript support and HTML template generation.

---
+++ task_1/js/main.ts
@@ -0,0 +1,35 @@
+// Define the Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+  }
+  
+  // Extend Teacher interface to create Directors
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  // Create the example teacher
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
+  // Create the example director
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
